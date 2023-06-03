973f5e6f20e86283dfeaddfacc920ced3772490d
Hoa Ngo <hoango1607@gmail.com>
2023-06-03 09:56:54 +0930

    feat: add neosolarized colorscheme

--- lvim/config.lua
+++ lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-lvim.colorscheme = "onedark"
+lvim.colorscheme = "neosolarized"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -295,7 +295,49 @@ lvim.plugins = {
       },
     }
   },
+  {
+    'tjdevries/colorbuddy.nvim'
+  },
+  {
+    'svrana/neosolarized.nvim',
+    config = function()
+      require('neosolarized').setup {
+        comment_italics = true,
+        background_set = false
+      }
+    end
+  }
 }
+
+-- local cb = require "colorbuddy"
+-- local colors = cb.colors
+-- local Color = cb.Color
+-- local Group = cb.Group
+-- local groups = cb.groups
+-- local styles = cb.styles
+
+-- Color.new('white', '#ffffff')
+-- Color.new('black', '#000000')
+-- Group.new('Normal', colors.base1, colors.NONE, styles.NONE)
+-- Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
+-- Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
+-- Group.new('Visual', colors.none, colors.base03, styles.reverse)
+
+-- local cError = groups.Error.fg
+-- local cInfo = groups.Information.fg
+-- local cWarn = groups.Warning.fg
+-- local cHint = groups.Hint.fg
+
+-- Group.new("DiagnosticVirtualTextError", cError, cError:dark():dark():dark():dark(), styles.NONE)
+-- Group.new("DiagnosticVirtualTextInfo", cInfo, cInfo:dark():dark():dark(), styles.NONE)
+-- Group.new("DiagnosticVirtualTextWarn", cWarn, cWarn:dark():dark():dark(), styles.NONE)
+-- Group.new("DiagnosticVirtualTextHint", cHint, cHint:dark():dark():dark(), styles.NONE)
+-- Group.new("DiagnosticUnderlineError", colors.none, colors.none, styles.undercurl, cError)
+-- Group.new("DiagnosticUnderlineWarn", colors.none, colors.none, styles.undercurl, cWarn)
+-- Group.new("DiagnosticUnderlineInfo", colors.none, colors.none, styles.undercurl, cInfo)
+-- Group.new("DiagnosticUnderlineHint", colors.none, colors.none, styles.undercurl, cHint)
+
+-- Group.new("HoverBorder", colors.yellow, colors.none, styles.NONE)
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- vim.api.nvim_create_autocmd("BufEnter", {
