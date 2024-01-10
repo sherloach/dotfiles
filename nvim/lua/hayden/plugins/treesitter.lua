22bd1990b3532b56c4d92d3218d1f71ee45fd7c5
Hayden Ngo <hoango1607@gmail.com>
2024-01-10 16:48:13 +1030

    feat: add obsidian

--- nvim/lua/hayden/plugins/treesitter.lua
+++ nvim/lua/hayden/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "javascript", "typescript", "html", "rust" },
-            sync_install = false,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true }
-        })
-    end
+    configs.setup({
+      ensure_installed = { "c", "lua", "javascript", "typescript", "html", "rust", "markdown", "markdown_inline" },
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true }
+    })
+  end
 }
