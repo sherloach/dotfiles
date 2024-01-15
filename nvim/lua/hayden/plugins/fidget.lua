11fcd09e9a15dd09ebb4ccd095acdd815642c011
Hayden Ngo <hoango1607@gmail.com>
2024-01-15 21:42:14 +1030

    chore: update nvim

--- nvim/lua/hayden/plugins/fidget.lua
+++ nvim/lua/hayden/plugins/fidget.lua
@@ -1,6 +1,8 @@
 return {
   "j-hui/fidget.nvim",
   config = function()
-    require("fidget").setup({})
+    require("fidget").setup({
+      notification = { window = { winblend = 0 } },
+    })
   end
 }
---
+++ nvim/lua/hayden/plugins/neo-tree.lua
@@ -0,0 +1,109 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  event = "VeryLazy",
+  keys = {
+    { "<leader>e",     ":Neotree toggle right<CR>", silent = true, desc = "Right File Explorer" },
+    { "<leader><tab>", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      popup_border_style = "single",
+      enable_git_status = true,
+      enable_modified_markers = true,
+      enable_diagnostics = true,
+      sort_case_insensitive = true,
+      default_component_configs = {
+        indent = {
+          with_markers = false,
+          with_expanders = true,
+        },
+        modified = {
+          symbol = " ",
+          highlight = "NeoTreeModified",
+        },
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "",
+          folder_empty_open = "",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = "",
+            deleted = "",
+            modified = "",
+            renamed = "",
+            -- Status type
+            untracked = "",
+            ignored = "",
+            unstaged = "",
+            staged = "",
+            conflict = "",
+          },
+        },
+      },
+      window = {
+        position = "right",
+        -- width = 35,
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
+        mappings = {
+          ["o"] = { "open", nowait = true },
+          ["oc"] = "none",
+          ["od"] = "none",
+          ["og"] = "none",
+          ["om"] = "none",
+          ["on"] = "none",
+          ["os"] = "none",
+          ["ot"] = "none",
+        }
+      },
+      filesystem = {
+        use_libuv_file_watcher = true,
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules",
+          },
+          never_show = {
+            ".DS_Store",
+            "thumbs.db",
+          },
+        },
+        follow_current_file = {
+          enabled = true,          -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      },
+      event_handlers = {
+        {
+          event = "neo_tree_window_after_open",
+          handler = function(args)
+            if args.position == "left" or args.position == "right" then
+              vim.cmd("wincmd =")
+            end
+          end,
+        },
+        {
+          event = "neo_tree_window_after_close",
+          handler = function(args)
+            if args.position == "left" or args.position == "right" then
+              vim.cmd("wincmd =")
+            end
+          end,
+        },
+      },
+    })
+  end,
+}
