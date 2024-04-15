d91d0d7128f2385a3ccc202cd905bf7169b5fb8a
Hayden Ngo <hoango1607@gmail.com>
2024-04-15 17:11:11 +0930

    chore: update karabiner

--- nvim/lua/hayden/plugins/harpoon.lua
+++ nvim/lua/hayden/plugins/harpoon.lua
@@ -7,9 +7,8 @@ return {
     vim.keymap.set("n", "<leader>a", mark.add_file)
     vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-    vim.keymap.set("n", "<C-m>", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<C-t>", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+    vim.keymap.set("n", "<C-n>", function() ui.nav_file(1) end)
+    vim.keymap.set("n", "<C-m>", function() ui.nav_file(2) end)
+    vim.keymap.set("n", "<C-,>", function() ui.nav_file(3) end)
   end
 }
