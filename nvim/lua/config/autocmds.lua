e95e887b79f7c8c1a611cb89fab2b3e51f93f980
Hayden Ngo <hoango1607@gmail.com>
2024-04-27 21:10:07 +0930

    chore: config floating popup width

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,3 +4,12 @@
 
 -- trim white space
 vim.api.nvim_create_autocmd("BufWritePre", { command = "%s/\\s\\+$//e" })
+
+-- config floating popup's width
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or "single"
+  opts.max_width = opts.max_width or 90
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
