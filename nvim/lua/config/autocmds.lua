8a3b81b34179ddcdb72edd16ee662cc387b68c2b
Hayden Ngo <hoango1607@gmail.com>
2024-04-30 22:45:12 +0930

    chore: change a bit

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -10,6 +10,6 @@ local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
 function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
   opts = opts or {}
   opts.border = opts.border or "single"
-  opts.max_width = opts.max_width or 90
+  opts.max_width = opts.max_width or 96
   return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
