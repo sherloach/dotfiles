3f2f009a01500ab63dba1f766405335d4ebaeffd
Hayden Ngo <hoango1607@gmail.com>
2024-08-09 21:16:29 +0930

    chore: update :)

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -10,7 +10,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "kanagawa" } },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "dracula-soft" } },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.rust" },
