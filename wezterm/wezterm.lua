4ad1126edad69db80e68080205d4102db781209e
Hayden Ngo <hoango1607@gmail.com>
2024-09-11 23:17:26 +0930

    chore: update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("Fira Code")
+config.font = wezterm.font("Fira Code IF")
 config.font_size = 12.0
 
 return config
